42ada351b61919ba042a126c775bd6ab7c31e331
mweasel <melanie.segard@hotmail.fr>
2016-01-02 21:36:02 +0100

    Javadoc de la classe Download

--- Https/src/Download.java
+++ Https/src/Download.java
@@ -1,5 +1,3 @@
-
-
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -10,11 +8,28 @@ import java.nio.file.Paths;
 
 import javax.swing.AbstractAction;
 
+/**
+ * Création de la classe Download qui hérite de la classe AbstractAction du package javax.swing pour réaliser 
+ * le téléchargement d'un fichier texte puis réaliser des opérations dessus.
+ * @author Jonathan, Frédéric, Anthony et Mélanie
+ *
+ */
+
 public class Download extends AbstractAction {
 	String namefile = null;
+	
+	/**
+	 * Téléchargement du fichier texte.
+	 * @param texte est le texte à télécharger.
+	 */
 	public Download(String texte){
 		super(texte);
 	}
+	
+	/**
+	 * Méthode permettant de 
+	 * @param e est un évènement qui indique qu'une action de composant définie est produite.
+	 */
 	
 	public void actionPerformed(ActionEvent e) { 
 		
@@ -26,18 +41,27 @@ public class Download extends AbstractAction {
 		try {
 			fout.createNewFile();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		ishere(fi);
 		copyFile(fi,fout);
-
 	} 
+	
+	/**
+	 * Méthode permettant d'obtenir le lien du fichier téléchargé.
+	 * @return namefile qui est le chemin du fichier téléchargé
+	 */
 	
 	public String getLink(){
 		return namefile;
 	}
 	
+	/**
+	 * Méthode permettant de savoir si le fichier existe ou non.
+	 * @param file est le fichier dont on veut connaître l'existence ou non
+	 * @return true si le fichier existe
+	 * @return false si le fichier n'existe pas
+	 */
 	
 	public boolean ishere(File file){
 		File f =new File("server/" + file.getName());
@@ -50,37 +74,38 @@ public class Download extends AbstractAction {
 		}
 	}
 
-			public  void copyFile(File source1, File dest1){
-				if(ishere(source1)){
-					File source =new File("server/" + source1.getName());
-					File dest =new File("client/" + dest1.getName());
-				try{
-					// Declaration et ouverture des flux
-					java.io.FileInputStream sourceFile = new java.io.FileInputStream(source);
-			 
-					try{
-						java.io.FileOutputStream destinationFile = null;
-			 
-						try{
-							destinationFile = new FileOutputStream(dest);
-			 
-							// Lecture par segment de 0.5Mo 
-							byte buffer[] = new byte[512 * 1024];
-							int nbLecture;
-			 
-							while ((nbLecture = sourceFile.read(buffer)) != -1){
-								destinationFile.write(buffer, 0, nbLecture);
-							}
-						} finally {
-							destinationFile.close();
+	/**
+	 * Méthode permettant de copier un fichier source donc du server vers un fichier destination donc vers un client
+	 * @param source1 est le fichier provenant du server et à copier 
+	 * @param dest1 est le fichier copié dans le client
+	 */
+	
+	public  void copyFile(File source1, File dest1){
+		if(ishere(source1)){
+			File source =new File("server/" + source1.getName());
+			File dest =new File("client/" + dest1.getName());
+		try{
+			// Declaration et ouverture des flux
+			java.io.FileInputStream sourceFile = new java.io.FileInputStream(source);
+			try{
+				java.io.FileOutputStream destinationFile = null;
+			 		try{
+						destinationFile = new FileOutputStream(dest);
+						// Lecture par segment de 0.5Mo 
+						byte buffer[] = new byte[512 * 1024];
+						int nbLecture;
+						while ((nbLecture = sourceFile.read(buffer)) != -1){
+							destinationFile.write(buffer, 0, nbLecture);
 						}
 					} finally {
-						sourceFile.close();
+						destinationFile.close();
 					}
-				} catch (IOException e){
-					e.printStackTrace();
-				}
-
+			} finally {
+				sourceFile.close();
 			}
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		}
 	}
 }
